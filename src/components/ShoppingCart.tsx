8cd2817c6f4d9eba0ccb3b9693ed7e95033ba620
ginopastran <ginopastran@gmail.com>
2025-05-14 12:34:16 -0300

    -Using POS_ID from env

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1364,7 +1364,7 @@ export default function ShoppingCart() {
         descripcion: `Compra de ${orderItems.length} productos`,
         vendedorId: user.id,
         sucursalId: user.sucursalId,
-        externalPosId: "POS1747232351167", // El POS ID que ya está configurado
+        externalPosId: process.env.VITE_POS_ID, // El POS ID que ya está configurado
         items: orderItems,
       };
 
@@ -1933,7 +1933,7 @@ export default function ShoppingCart() {
         descripcion: `Pago parcial de $${qrAmount.toFixed(2)}`,
         vendedorId: userId,
         sucursalId: sucursalId,
-        externalPosId: "POS1747232351167",
+        externalPosId: process.env.VITE_POS_ID,
         // Enviar solo un item que representa el pago parcial
         items: [singleQrItem],
         // Metadatos adicionales
